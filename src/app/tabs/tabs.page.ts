052168a31c7021565f704f1d83bdb7e1dc9f3cd9
Nico Obregon <nmo@Nicolas-MacBook-Pro.local>
2026-04-21 12:32:19 -0300

    fix: tab i18n

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -2,6 +2,7 @@ import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { homeOutline, chatbubbleEllipsesOutline, personOutline } from 'ionicons/icons';
+import { I18nService } from '../i18n/i18n.service';
 
 @Component({
   selector: 'app-tabs',
@@ -11,6 +12,7 @@ import { homeOutline, chatbubbleEllipsesOutline, personOutline } from 'ionicons/
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
+  protected i18n = inject(I18nService);
 
   constructor() {
     addIcons({ homeOutline, chatbubbleEllipsesOutline, personOutline });
