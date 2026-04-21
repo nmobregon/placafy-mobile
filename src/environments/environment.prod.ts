3f51ae8bb4221e5fd043cfba03b30db19af83584
Nico Obregon <nmo@Nicolas-MacBook-Pro.local>
2026-04-21 11:17:14 -0300

    updated api domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://80.241.217.93:3000',
+  apiBaseUrl: 'https://placafy.mooo.com',
   publicApiKey: 'pk_39f7d16a2bcb4f97a4e53e7a',
 };
