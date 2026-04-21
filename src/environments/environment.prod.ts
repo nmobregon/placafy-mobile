124e9a88c051f7284b252c4b53b15a1479755053
Nico Obregon <nmo@Nicolas-MacBook-Pro.local>
2026-04-20 21:37:57 -0300

    updated prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:3000',
+  apiBaseUrl: 'http://80.241.217.93:3000',
   publicApiKey: 'pk_39f7d16a2bcb4f97a4e53e7a',
 };
